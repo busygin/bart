2a0dc7552fe6a252cef9c8523579c23bf406fb38
Stanislav Busygin <busygin@gmail.com>
2015-12-27 08:05:54 -0500

    Contiguous arrays

--- ComputeBart.py
+++ ComputeBart.py
@@ -9,10 +9,15 @@ class ComputeBart(object):
         self.bart_regressor.set_run_params(nd_=nd, lambda_=lambda_, burn_=burn, m_=m, nc_=nc, nu_=nu, kfac_=kfac)
 
     def fit_and_predict(self, X, y, X1):
+        X_ = X if X.flags['C_CONTIGUOUS'] else np.ascontiguousarray(X)
+        y_ = y if y.flags['C_CONTIGUOUS'] else np.ascontiguousarray(y)
+        X1_ = X1 if X1.flags['C_CONTIGUOUS'] else np.ascontiguousarray(X1)
         y1 = np.empty(shape=X1.shape[0], dtype=float)
-        self.bart_regressor.set_insample_matrix(X)
-        self.bart_regressor.set_insample_target(y)
-        self.bart_regressor.set_outsample_matrix(X1)
+
+        self.bart_regressor.set_insample_matrix(X_)
+        self.bart_regressor.set_insample_target(y_)
+        self.bart_regressor.set_outsample_matrix(X1_)
         self.bart_regressor.set_outsample_target(y1)
         self.bart_regressor.fit()
+
         return y1
